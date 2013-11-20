bac2548f3d85c9c6b780f539b5de7cbdb38b603f
Kyle Kelley <kyle.kelley@rackspace.com>
2013-11-20 11:03:18 -0600

    Move queues to rackspace module

--- stackslurp/main.py
+++ stackslurp/main.py
@@ -1,39 +1,10 @@
-import json
 import time
-import uuid
 
-from urlparse import urljoin
-
-import requests
 import yaml
 
 from . import stackexchange
 from .utils import chunks
 from .rackspace import Rackspace
-
-class PerilEvent(object):
-    '''
-    Dummy object to create a proper peril event
-
-    Totally overkill at this point
-    '''
-
-    def __init__(self, url, tags, reporter):
-        self.url = url
-        self.tags = tags
-        self.reporter = reporter
-
-    def to_dict(self):
-        return {"url": self.url,
-                "tags": self.tags,
-                "reporter": self.reporter}
-
-    def to_json(self):
-        return json.dumps(self.to_dict())
-
-    def __repr__(self):
-        return str(self.to_json())
-
 
 def main():
 
@@ -49,41 +20,30 @@ def main():
     username = config['rackspace']['username']
     api_key = config['rackspace']['api_key']
 
+    queue_endpoint = config['rackspace']['queue_endpoint']
+    queue = config['queue']
+
     # Completely arbitrary start point
-    since = int(time.time() - 3600*48)
+    since = int(time.time() - 60*60*24*7)
 
     # Get all the questions since
     questions = stackexchange.search_questions(since, tags, site, stackexchange_key)
 
     # Create events
-    events = [PerilEvent(question["link"], question["tags"], "stackslurp")
-              for question in questions]
+    events = [ {"url": question["link"],
+                "tags": question["tags"],
+                "reporter": "stackslurp"} for question in questions]
 
     print(events)
 
+    # Authenticate with Rackspace
     rack = Rackspace(username, api_key)
     rack.auth()
 
     # Now we're authenticated, time to send on to a queue
-    queue_endpoint = config['rackspace']['queue_endpoint']
-
-    # Every queue client needs a unique ID
-    client_id = str(uuid.uuid4())
-
-    post_message_url = urljoin(queue_endpoint, "/v1/queues/{}/messages".format(config['queue']))
-    headers = {'Content-type': 'application/json', "Client-ID": client_id, "X-Auth-Token": rack.token}
-
-    # Break events up into chunks of 10, per queue limit (configurable?)
-    # TODO: Figure out if more than 10 messages can be sent at one time a different way
+    # Break events up into chunks of 10, per arbitrary queue limit
     for event_chunk in chunks(events, 10):
-      data = [{"ttl": 300, "body": event.to_dict()} for event in event_chunk]
-
-      print(data)
-      print(json.dumps(data))
-
-      resp = requests.post(post_message_url, data=json.dumps(data), headers=headers)
-      print(resp.reason)
-      print(resp.content)
+      rack.enqueue(event_chunk, queue, queue_endpoint)
 
 if __name__ == "__main__":
     main()
--- stackslurp/rackspace.py
+++ stackslurp/rackspace.py
@@ -7,12 +7,16 @@ This module helps with auth and working with queues.
 import json
 import uuid
 
+from urlparse import urljoin
+
 import requests
 
 class Rackspace(object):
     def __init__(self,username,api_key):
         self.username = username
         self.api_key = api_key
+
+        # Generate a client ID for Queue usage
         self.client_id = str(uuid.uuid4())
 
     def auth(self):
@@ -40,3 +44,19 @@ class Rackspace(object):
         identity_data = resp.json()
         self.token = identity_data['access']['token']['id']
 
+
+    def enqueue(self, messages, queue, endpoint, ttl=300):
+        '''Messages must be JSON-serializable dicts
+        '''
+        post_message_url = urljoin(endpoint, "/v1/queues/{}/messages".format(queue))
+        headers = {'Content-type': 'application/json',
+                   "Client-ID": self.client_id,
+                   "X-Auth-Token": self.token}
+
+        data = [{"ttl":ttl, "body": message} for message in messages]
+
+        print(json.dumps(data))
+
+        resp = requests.post(post_message_url, data=json.dumps(data), headers=headers)
+        print(resp.reason)
+        print(resp.content)
