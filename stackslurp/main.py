a8ab167490a5b5104c5f6ac8f853a18a4b508665
Kyle Kelley <kyle.kelley@rackspace.com>
2013-11-20 21:50:11 -0600

    Handle repeated runs, waiting

--- stackslurp/main.py
+++ stackslurp/main.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+'''
+Just a simple script designed to read from a configuration file and use
+stackslurp's modules to read from StackExchange and send data on to
+CloudQueues.
+'''
+
 import time
 
 import yaml
@@ -26,29 +34,47 @@ def main():
     queue_endpoint = config['rackspace']['queue_endpoint']
     queue = config['queue']
 
-    # Completely arbitrary start point
-    since = int(time.time() - 60 * 60 * 24 * 7)
+    # Timing configuration
+    wait_time = int(config.get('wait_time', 60 * 10))
 
-    # Get all the questions since
-    questions = stackexchange.search_questions(since, tags,
-                                               site, stackexchange_key)
+    starting_since = config.get('starting_since', time.time() - 60 * 60 * 24)
+    since = int(starting_since)
 
-    # Create events
-    events = [{"url": question["link"],
-              "tags": question["tags"],
-              "incident_date": question["creation_date"],
-              "reporter": "stackslurp"} for question in questions]
-
-    print(events)
-
-    # Authenticate with Rackspace
+    # Get our Rackspace API set up
     rack = Rackspace(username, api_key)
-    rack.auth()
 
-    # Now we're authenticated, time to send on to a queue
-    # Break events up into chunks of 10, per arbitrary queue limit
-    for event_chunk in chunks(events, 10):
-        rack.enqueue(event_chunk, queue, queue_endpoint)
+    while(True):
+        # Get all the questions that have been asked with our tags going back
+        # to since on site
+        questions = stackexchange.search_questions(since, tags,
+                                                   site, stackexchange_key)
+
+        if(len(questions) > 0):
+            # Track the last creation date to get new questions on the next run
+            # `since` is >= in the stackexchange call, so we go 1 second later
+            # so the slurper doesn't keep reporting the same last event over
+            # and over.
+            since = questions[0]["creation_date"] + 1
+
+        # Create events
+        events = [{"url": question["link"],
+                  "tags": question["tags"],
+                  "incident_date": question["creation_date"],
+                  "reporter": "stackslurp"} for question in questions]
+
+        print("Events:")
+        print(events)
+
+        # Authenticate with Rackspace (get a new token every time we loop here)
+        rack.auth()
+
+        # Now we're authenticated, time to send on to a queue
+        # Break events up into chunks of 10, per arbitrary queue limit
+        for event_chunk in chunks(events, 10):
+            rack.enqueue(event_chunk, queue, queue_endpoint)
+
+        # Another arbitrary time
+        time.sleep(wait_time)
 
 if __name__ == "__main__":
     main()
