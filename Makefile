f05389a36b9f127901acdb9d18c4f1e5fb99997d
Kyle Kelley <kyle.kelley@rackspace.com>
2013-12-27 18:55:13 -0600

    Add tests for SlurpConfig

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ test:
 	py.test
 
 coverage:
-	py.test --verbose --cov-report term --cov=stackslurp test_stackslurp.py
+	py.test --verbose --cov-report term --cov=stackslurp tests/test_stackslurp.py
--- tests/test_stackslurp.py
+++ tests/test_stackslurp.py
@@ -6,9 +6,11 @@ import unittest
 import os
 import random
 import json
+import StringIO
 
 import httpretty
 import pytest
+import yaml
 
 import stackslurp
 import stackslurp.main
@@ -38,6 +40,43 @@ class StackslurpTestCase(unittest.TestCase):
         assert num_chunks.next() == [9]
 
         # TODO Add doctests
+
+class SlurpConfigTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_config(self):
+
+        config_dict = {
+                "stackexchange_key": 'THE_SE_KEY',
+                "tags": ["python", "ruby"],
+                "sites": ["stackoverflow", "serverfault"],
+                "rackspace": {"username": "user",
+                              "api_key": "rackspace_api",
+                              "queue_endpoint":
+                                "https://dfw.queues.api.rackspacecloud.com/v1/"
+                             },
+                "queue": "testing",
+                "wait_time": 300
+        }
+
+        fh = StringIO.StringIO(yaml.safe_dump(config_dict))
+
+        SlurpConfig = stackslurp.main.SlurpConfig
+
+        config = SlurpConfig(fh)
+
+        assert config.stackexchange_key == config_dict["stackexchange_key"]
+        assert config.tags == config_dict["tags"]
+        assert config.sites == config_dict["sites"]
+        assert config.username == config_dict["rackspace"]["username"]
+        assert config.api_key == config_dict["rackspace"]["api_key"]
+        assert config.queue_endpoint == config_dict["rackspace"]["queue_endpoint"]
+        assert config.queue == config_dict["queue"]
+        assert config.wait_time == config_dict["wait_time"]
+
 
 if __name__ == "__main__":
     #testSuite = unittest.TestSuite()
